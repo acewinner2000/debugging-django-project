922eced153aafc85161769b0bf157a8f01aebfc1
acewinner2000 <ali.ghiasabadi.2000@gmail.com>
2025-02-10 14:02:37 +0330

    1

--- services/views.py
+++ services/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def services(request):
-    pass
+    service_b=Services_a.objects.filter(status=True)
+    service=Services.objects.filter(status=True)
+    context={
+        'service':service,
+        'services':service_b
+    }
+    return render(request,'services/services.html',context=context)
